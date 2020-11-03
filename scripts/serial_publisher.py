e0a2fefffd321e9ba302f987dcafc0032d49cee5
MichaelJNielsen <mjn@es.aau.dk>
2020-11-03 12:51:09 +0100

    troubleshooting

--- scripts/serial_publisher.py
+++ scripts/serial_publisher.py
@@ -16,71 +16,8 @@ def keyboardInterruptHandler(signal,frame):
     exit(0)
 
 signal.signal(signal.SIGINT,keyboardInterruptHandler)
-
-def read_from_serial1():
-    global latest_received1, buffer_bytes1
-    serial_data = []
-    bytesToRead = ser1.inWaiting()
-    temp_bytes = ser1.read(bytesToRead)
-    buffer_bytes1 = buffer_bytes1 + temp_bytes
-    buffer_string = buffer_bytes1.decode()
-    lines = buffer_string.split('\r\n')
-    filter_lines = list(filter(None,lines))
-    if len(filter_lines) > 1:
-        latest_received1 = filter_lines[-2]
-        buffer_bytes1 = temp_bytes
-    else:
-        print("Not enough serial input, using last available")
-    splitline = latest_received1.split(',')
-    for x in splitline:
-        serial_data.append(float(x))
-    if len(serial_data) < 10:
-        print("exitting due to error on serial line 1")
-        exit(0)
-    else:
-        return(serial_data)
-
-def read_from_serial2():
-    global latest_received2, buffer_bytes2
-    serial_data = []
-    bytesToRead = ser2.inWaiting()
-    print("bytesToRead")
-    print(bytesToRead)
-    temp_bytes = ser2.read(bytesToRead)
-    print("temp_bytes")
-    print(temp_bytes)
-    buffer_bytes2 = buffer_bytes2 + temp_bytes
-    print("buffer_bytes2")
-    print(buffer_bytes2)
-    buffer_string = buffer_bytes2.decode()
-    print("buffer_string")
-    print(buffer_string)
-    lines = buffer_string.split('\r\n')
-    print("lines")
-    print(lines)
-    filter_lines = list(filter(None,lines))
-    print("filter lines")
-    print(filter_lines)
-    if len(filter_lines) > 1:
-        latest_received2 = filter_lines[-2]
-        buffer_bytes2 = temp_bytes
-    else:
-        print("Not enough serial input, using last available")
-    print("latest received")
-    print(latest_received2)
-    splitline = latest_received2.split(',')
-    print("splitline")
-    print(splitline)
-    for x in splitline:
-        serial_data.append(float(x))
-    print("serial_data")
-    print(serial_data)
-    if len(serial_data) < 10:
-        exit(0)
-    else:
-        return(serial_data)
         
-def read_from_serial1_v2():
+def read_from_serial1():
     global latest_received1, buffer_bytes1
     serial_data = []
     bytesToRead = ser1.inWaiting()
@@ -105,7 +42,7 @@ def read_from_serial1_v2():
     else:
         return(serial_data)
 
-def read_from_serial2_v2():
+def read_from_serial2():
     global latest_received2, buffer_bytes2
     serial_data = []
     bytesToRead = ser2.inWaiting()
@@ -144,8 +81,9 @@ if __name__ == '__main__':
 
     while True:
         beginTime = time.time()
-        serial_data1 = read_from_serial1_v2()
-        serial_data2 = read_from_serial2_v2()
+        #serial_data1 = read_from_serial1()
+        serial_data1 = [1,2,3,4,5,6,7,8,9,10]
+        serial_data2 = read_from_serial2()
         
         msg1.time_stamp = serial_data1[0]
         msg1.acc_x = serial_data1[1]
